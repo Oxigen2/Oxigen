46ca04a6fa99bbedcaae46b3b67fa6b4d488e26b
unknown <Ashter@.(none)>
2011-04-05 11:15:17 +0100

    Client tools for obfuscation and digital signing added

---
+++ build/Client tools/Certificates/sign.bat
@@ -0,0 +1,7 @@
+"c:\Program Files\Microsoft SDKs\Windows\v6.0A\bin\signtool.exe" sign /f "c:\Oxigen\oxigen II Ltd Code Signing Cert.pfx" /p 5513070 /t http://timestamp.verisign.com/scripts/timstamp.dll .\*.exe
+
+"c:\Program Files\Microsoft SDKs\Windows\v6.0A\bin\signtool.exe" sign /f "c:\Oxigen\oxigen II Ltd Code Signing Cert.pfx" /p 5513070 /t http://timestamp.verisign.com/scripts/timstamp.dll .\*.msi
+
+"c:\Program Files\Microsoft SDKs\Windows\v6.0A\bin\signtool.exe" sign /f "c:\Oxigen\oxigen II Ltd Code Signing Cert.pfx" /p 5513070 /t http://timestamp.verisign.com/scripts/timstamp.dll .\*.dll
+
+"c:\Program Files\Microsoft SDKs\Windows\v6.0A\bin\signtool.exe" sign /f "c:\Oxigen\oxigen II Ltd Code Signing Cert.pfx" /p 5513070 /t http://timestamp.verisign.com/scripts/timstamp.dll .\*.scr
