2d08ae2b3cdc021d41ee27a45b7779f67e9a2da4
oxigen2 <ashter_chomoko@hotmail.com>
2011-05-03 18:58:27 +0100

    Image Scrapper temp

---
+++ app/Oxigen.Core/TemplateMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oxigen.Core
+{
+    public class TemplateParameter
+    {
+        public string Name { get; set; }
+        public string DefaultText { get; set; }
+    }
+
+    public class TemplateSchema
+    {
+        
+    }
+
+    public class ItemParam
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+    }
+}
---
+++ tests/Oxigen.Tests/Oxigen.Core/ScraperTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SharpArch.Testing;
+using SharpArch.Testing.NUnit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using NUnit.Framework;
+
+namespace Tests.Oxigen.Core
+{
+    [TestFixture]
+    public class ScraperTests
+    {
+        [Test]
+        public void CanExtractImageForMediaWeak()
+        {
+            var hw = new HtmlWeb();
+            var doc = hw.Load("http://www.mediaweek.co.uk/news/rss/1067882/IPC-Global-amplify-Ford-Fiestas-summer/");
+            var node = doc.DocumentNode.SelectSingleNode("//div[@id=\"articleImageHolder\"]//img");
+            var src = node.Attributes["src"].Value;
+            src.ShouldEqual(
+                "http://cached.imagescaler.hbpl.co.uk/resize/scaleToFit/427/285/?sURL=http://offlinehbpl.hbpl.co.uk/news/OWM/B662E6EC-D287-2E41-A137FE30F1A23DD6.JPG");
+        }
+    }
+}
