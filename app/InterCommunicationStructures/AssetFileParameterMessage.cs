41484fc997fa17617230374f2caf2b4b66f59b56
Michali <michali@gmail.com>
2011-05-18 18:16:07 +0100

    Content Exchanger amended to work with the CDN for slide retrieval

--- app/InterCommunicationStructures/AssetFileParameterMessage.cs
+++ app/InterCommunicationStructures/AssetFileParameterMessage.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ServiceModel;
-
+﻿using System.ServiceModel;
+//TODO: possibly remove once CDN is implemented
 namespace InterCommunicationStructures
 {
   /// <summary>
