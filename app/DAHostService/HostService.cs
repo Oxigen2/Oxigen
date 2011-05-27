ed9ebdaee016edfce339d30179d4371e57849ec0
Michali K <michali@gmail.com>
2011-05-27 11:35:22 +0100

    Cleaned up unused usings

--- app/DAHostService/HostService.cs
+++ app/DAHostService/HostService.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
 using System.ServiceModel;
 using OxigenIIAdvertising.Services;
-using OxigenIIAdvertising.ServiceContracts.DAServices;
 
 namespace DAHostService
 {
