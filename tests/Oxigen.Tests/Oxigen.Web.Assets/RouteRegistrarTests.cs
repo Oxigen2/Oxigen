d38c6a7d9f081f2102ab988cabfdefb7a9869a31
Michali K <michali@gmail.com>
2011-06-06 19:19:58 +0100

    Tests for the Non-WCF User data marshaller

---
+++ tests/Oxigen.Tests/Oxigen.Web.Assets/RouteRegistrarTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Routing;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+using Oxigen.Web.Assets;
+using AssertionException = MvcContrib.TestHelper.AssertionException;
+
+namespace Tests.Oxigen.Web.Assets
+{
+  [TestFixture]
+  public class RouteRegistrarTests
+  {
+    #region Public Methods
+
+    [SetUp]
+    public void SetUp()
+    {
+      RouteTable.Routes.Clear();
+      RouteRegistrar.RegisterRoutesTo(RouteTable.Routes);
+    }
+    
+    [Test]
+    public void CanVerifyRouteMaps()
+    {
+      "~/slide/GUID_A.jpg".Route().ShouldMapTo<ServecontentController>(x => x.Slide("GUID_A.jpg"));
+      "~/advert/GUID_A.jpg".Route().ShouldMapTo<ServecontentController>(x => x.Advert("GUID_A.jpg"));
+    }
+
+    #endregion
+  }
+}
