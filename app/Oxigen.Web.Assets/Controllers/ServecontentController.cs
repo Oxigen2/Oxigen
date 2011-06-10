98d6ce8a410b0926f607aa1ff68d94943b648273
Michali K <michali@gmail.com>
2011-06-10 11:04:20 +0100

    Content Exchanger connects to CDN server for the assets.
    Log4Net added to Oxigen.Web.Assets

--- app/Oxigen.Web.Assets/Controllers/ServecontentController.cs
+++ app/Oxigen.Web.Assets/Controllers/ServecontentController.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Web.Mvc;
-
+﻿using System.Web.Mvc;
+using log4net;
 
 namespace Oxigen.Web.Assets
 {
   [HandleError]
   public class ServecontentController : Controller
   {
+    private static readonly ILog _logger = LogManager.GetLogger("FileLogger");
+
     [HttpGet]
     public ActionResult Slide(string filename)
     {
       string path = FileRepositoryHelper.GetAssetFullPath(filename);
 
       if (!System.IO.File.Exists(path))
+      {
+        _logger.Debug(path + " does not exist.");
         return HttpNotFound("File not found");
+      }
+
+      _logger.Debug("Retrieving " + path);
 
       return new FilePathResult(path, "bad/type");
     }
