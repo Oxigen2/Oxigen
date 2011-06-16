97decc02adab784872b2d01ce1bc76ebafa6702c
Ashter Chomoko <ashter@chomoko.com>
2011-06-16 13:16:55 +0100

    IPlayer interface

---
+++ app/OxigenIIScreenSaver/OxigenIIScreenSaver/IPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OxigenIIAdvertising.ScreenSaver
+{
+    public interface IPlayer
+    {
+        void EnableSound(bool enableSound);
+        void Play();
+        Control Control {get;}
+    }
+}
