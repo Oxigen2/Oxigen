f2831a4264e6713cc0478a12bec7436fb94f8ea7
Michali K <michali@gmail.com>
2011-06-22 11:41:50 +0100

    changed setup code

--- app/Setup/ClientLoggers/ClientLogger.cs
+++ app/Setup/ClientLoggers/ClientLogger.cs
@@ -4,7 +4,7 @@ namespace Setup.ClientLoggers
 {
   public abstract class ClientLogger
   {
-    protected readonly string _loggingPartialUrl = "http://new.oxigen.net/log/setup3/";
+    protected readonly string _loggingPartialUrl = "http://new.oxigen.net/log/setup4/";
     protected string _userRef = null;
 
     protected delegate WebResponse WebResponseGetter();
