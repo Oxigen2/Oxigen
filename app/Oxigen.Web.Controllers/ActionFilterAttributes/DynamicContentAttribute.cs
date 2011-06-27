ce71a83e93c2296e3e6b2162d7c97526655b08ef
Michali K <michali@gmail.com>
2011-06-27 13:55:26 +0100

    LIVE - Channel subscriptions and demographics - RESTFul implementation and cache

---
+++ app/Oxigen.Web.Controllers/ActionFilterAttributes/DynamicContentAttribute.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oxigen.Web.Controllers.ActionFilterAttributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class DynamicContentAttribute : ActionFilterAttribute
+    {
+        private int _maxAgeInSeconds;
+        private string _eTag;
+        public int MaxAgeInSeconds
+        {
+            get { return _maxAgeInSeconds; }
+            set { _maxAgeInSeconds = value; }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpRequestBase request = filterContext.RequestContext.HttpContext.Request;
+            HttpResponseBase response = filterContext.RequestContext.HttpContext.Response;
+
+            if (filterContext.Result == null || !(filterContext.Result is FileContentResult))
+            {
+                base.OnActionExecuting(filterContext);
+                return;
+            }
+
+            var result = filterContext.Result as FileContentResult;
+
+            _eTag = Convert.ToBase64String(new System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(result.FileContents));
+
+            if (request.Headers["If-None-Match"] != null && request.Headers["If-None-Match"] == _eTag)
+            {
+                response.Write(DateTime.Now);
+                response.StatusCode = 304;
+                response.StatusDescription = "Not Modified";
+            }
+            else
+                base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Result != null && filterContext.Result is FileContentResult)
+                SetCaching(filterContext.HttpContext.Response);
+
+            base.OnActionExecuted(filterContext);
+        }
+
+        private void SetCaching(HttpResponseBase response)
+        {
+            response.ClearHeaders();
+            response.Cache.SetSlidingExpiration(true);
+            response.Cache.SetCacheability(HttpCacheability.Public);
+            response.Cache.SetMaxAge(TimeSpan.FromSeconds(_maxAgeInSeconds));
+        }
+    }
+}
---
+++ app/Oxigen.Web.Controllers/SubscriberInfoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Oxigen.Web.Controllers.ActionFilterAttributes;
+using OxigenIIAdvertising.AppData;
+using OxigenIIAdvertising.Demographic;
+using OxigenIIAdvertising.Services;
+using OxigenIIAdvertising.XMLSerializer;
+
+namespace Oxigen.Web.Controllers
+{
+    [HandleError]
+    public class SubscriberInfoController : Controller
+    {
+        [DynamicContent(MaxAgeInSeconds = 10800)] // 3 hours
+        public ActionResult Subscriptions(string id)
+        {
+            ChannelSubscriptions subscriptions = (new DAService("Oxigen")).GetUserChannelSubscriptions(null, id); // for backwards compatibility, unused userGUID param is set to null instead of creating another method and propagating it to the WCF contracts.
+
+            if (subscriptions.SubscriptionSet == null)
+                return HttpNotFound();
+
+            byte[] subscriptionsInBytes = Serializer.SerializeClearToByteArray(subscriptions);
+            var fileContentResult = new FileContentResult(subscriptionsInBytes, System.Net.Mime.MediaTypeNames.Application.Octet);
+            fileContentResult.FileDownloadName = "ss_channel_subscription_data.dat";
+            return fileContentResult;
+        }
+
+        [DynamicContent(MaxAgeInSeconds = 86400)] // 1 day
+        public ActionResult DemographicData(string id)
+        {
+            DemographicData demographicData = (new DAService("Oxigen")).GetUserDemographicData(id);
+
+            if (demographicData.Gender == null)
+                return HttpNotFound();
+
+            byte[] demographicDataInBytes = Serializer.SerializeClearToByteArray(demographicData);
+            var fileContentResult = new FileContentResult(demographicDataInBytes, System.Net.Mime.MediaTypeNames.Application.Octet);
+            fileContentResult.FileDownloadName = "ss_demo_data.dat";
+            return fileContentResult;
+        }
+    }
+}
