0c01121b8fdda20ebfb6296091763bdf048c2aa6
Michali K <michali@gmail.com>
2011-06-27 14:02:34 +0100

    Cleaned up code from DemographicData.cs

--- app/OxigenIIDemographicData/DemographicData.cs
+++ app/OxigenIIDemographicData/DemographicData.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using OxigenIIAdvertising.Exceptions;
 using System.Runtime.Serialization;
 
 namespace OxigenIIAdvertising.Demographic
