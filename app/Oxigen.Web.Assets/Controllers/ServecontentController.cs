989c3e3f3ec2c08146d612671bafdd2ace882c09
Michali K <michali@gmail.com>
2011-06-28 12:33:29 +0100

    LIVE - Modified caching

--- app/Oxigen.Web.Assets/Controllers/ServecontentController.cs
+++ app/Oxigen.Web.Assets/Controllers/ServecontentController.cs
@@ -7,30 +7,27 @@ namespace Oxigen.Web.Assets
   [HandleError]
   public class ServecontentController : Controller
   {
-    private static readonly ILog _logger = LogManager.GetLogger("FileLogger");
-
-    [FileCache(MaxAge = 31536000)] //one year
+    [OxigenFileCacheAttribute(MaxAge = 31536000)] //one year
     public ActionResult Slide(string filename)
     {
         string path = FileRepositoryHelper.GetAssetFullPath(filename);
         return new FilePathResult(path, System.Net.Mime.MediaTypeNames.Application.Octet);
     }
 
-      [FileCache(MaxAge = 1800)] //30 min
+    [OxigenFileCacheAttribute(MaxAge = 30)] // 30 secs
     public ActionResult Channel(string filename) {
         string path = FileRepositoryHelper.GetChannelDataFullPath(filename);
         return new FilePathResult(path, System.Net.Mime.MediaTypeNames.Application.Octet);
     }
-      [HttpGet, FileCache(MaxAge = 31536000)] //one year
+    [HttpGet, OxigenFileCacheAttribute(MaxAge = 31536000)] //one year
     public ActionResult Advert(string filename)
     {
       return Slide(filename);
     }
-      [FileCache(MaxAge = 10800)] //3 hours
+    [OxigenFileCacheAttribute(MaxAge = 10800)] //3 hours
     public ActionResult Data(string filename)
     {
         return new FilePathResult(FileRepositoryHelper.GetDataFullPath(filename), System.Net.Mime.MediaTypeNames.Application.Octet);
     }
-
   }
 }
--- app/Oxigen.Web.Assets/FileCacheAttribute.cs
+++ app/Oxigen.Web.Assets/FileCacheAttribute.cs
@@ -2,59 +2,39 @@
 using System.IO;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace Oxigen.Web.Assets
 {
     [AttributeUsage(AttributeTargets.Class |
                     AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
-    public class FileCacheAttribute : ActionFilterAttribute
+    public class OxigenFileCacheAttribute : ActionFilterAttribute
     {
         private int _maxAge = 1800;
 
         public int MaxAge { get { return _maxAge; } set { _maxAge = value; } } 
 
         //TODO: check file path does not contain ".."
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        public override void OnResultExecuting(ResultExecutingContext ctx)
         {
-            HttpRequestBase request = filterContext.RequestContext.HttpContext.Request;
-            HttpResponseBase response = filterContext.RequestContext.HttpContext.Response;
-            var result = filterContext.Result as FilePathResult;
+            HttpRequestBase request = ctx.RequestContext.HttpContext.Request;
+            HttpResponseBase response = ctx.RequestContext.HttpContext.Response;
+            var result = ctx.Result as FilePathResult;
             if (result == null)
             {
-                base.OnActionExecuting(filterContext);
+                base.OnResultExecuting(ctx);
                 return;
             }
 
             if (!File.Exists(result.FileName))
-            {          
-                response.Write(DateTime.Now);
+            {
                 response.StatusCode = 404;
                 response.StatusDescription = "Not Found";
+                ctx.Cancel = true;
                 return;
-
-            }
-            
-            if ((request.Headers["If-Modified-Since"] != null) &&  
-                 new FileInfo(result.FileName).LastWriteTimeUtc >  DateTime.Parse(request.Headers["If-Modified-Since"]))
-
-            {
-                response.Write(DateTime.Now);
-                response.StatusCode = 304;
-                //response.Headers.Add("Content-Encoding", "gzip");
-                response.StatusDescription = "Not Modified";
             }
 
-            else
-
-            {
-                base.OnActionExecuting(filterContext);
-            }
-        }
-
-        private void SetFileCaching(HttpResponseBase response, string fileName)
-
-        {
-            response.AddFileDependency(fileName);
+            response.AddFileDependency(result.FileName);
 
             //Expensive to great eTags for large files and is not really necessary since we are using LastModified Date
             //response.Cache.SetETagFromFileDependencies();
@@ -62,27 +42,22 @@ namespace Oxigen.Web.Assets
             response.Cache.SetLastModifiedFromFileDependencies();
 
             response.Cache.SetCacheability(HttpCacheability.Public);
+
             if (_maxAge > -1)
-                response.Cache.SetMaxAge(new TimeSpan(0, 0, _maxAge, 0));
+                response.Cache.SetMaxAge(new TimeSpan(0, 0, 0, _maxAge));
 
             response.Cache.SetSlidingExpiration(true);
-        }
-
-
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
-
-        {
-            var result = filterContext.Result as FilePathResult;
-
-            if (result != null)
-
+            
+            if ((request.Headers["If-Modified-Since"] != null) &&
+                 new FileInfo(result.FileName).LastWriteTime  <= DateTime.Parse(request.Headers["If-Modified-Since"]).AddSeconds(1))
             {
-                if (!string.IsNullOrEmpty(result.FileName) && (File.Exists(result.FileName)))
-
-                    SetFileCaching(filterContext.HttpContext.Response, result.FileName);
+                response.StatusCode = 304;
+                //response.Headers.Add("Content-Encoding", "gzip");
+                response.StatusDescription = "Not Modified";
+                ctx.Cancel = true;
             }
-
-            base.OnActionExecuted(filterContext);
+            else
+                base.OnResultExecuting(ctx);
         }
     }
 }
