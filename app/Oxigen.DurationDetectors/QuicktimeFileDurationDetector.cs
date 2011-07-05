66bcc9809aa8c3417e95156377f0f44204af26c3
Michali K <michali@gmail.com>
2011-07-05 18:03:40 +0100

    WMV and QT duration detection

--- app/Oxigen.DurationDetectors/QuicktimeFileDurationDetector.cs
+++ app/Oxigen.DurationDetectors/QuicktimeFileDurationDetector.cs
@@ -1,48 +1,81 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Oxigen.DurationDetectors
 {
     public class QuicktimeFileDurationDetector : IFileDurationDetector
     {
-        private const int MVHD_HEADER_NAME_LENGTH = 4;
+        private const int MVHD_HEADER_NAME_LENGTH = 5;
+        private const int USEFUL_BYTE_OFFSET = 12;
         private int _numBytesRead;
-        private int _numBytesToRead;
         private byte[] _bytes;
-
+        private bool _mvhdAtomHeaderNamePassed;
+        
         public double GetDurationInSeconds(string path)
         {
+            byte[] timeScaleBytes = new byte[4];
+            byte[] durationBasedOnTimeScaleBytes = new byte[4]; // In QT clips, time is specified in terms of the movie time scale
+            int usefulByteCount = 0;
+            int timeScaleBytesIndex = 0;
+            int durationBasedOnTimeScaleBytesIndex = 0;
+
             using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
             {
-                _numBytesToRead = (int)fs.Length;
-                 _bytes = new byte[_numBytesToRead];
-
-                while (_numBytesToRead > 0)
+                int fileStreamLength = (int)fs.Length;
+                _bytes = new byte[fileStreamLength];
+                
+                while (_numBytesRead < fileStreamLength)
                 {
-                    int n = fs.Read(_bytes, _numBytesRead, _numBytesToRead);
+                    int n = fs.Read(_bytes, _numBytesRead, 1);
 
                     if (n == 0)
                         break;
 
                     _numBytesRead += n;
-                    _numBytesToRead -= n;
 
-                    if (_numBytesRead <= MVHD_HEADER_NAME_LENGTH) continue;
-                    if (!MvhdAtomFound()) continue;
+                    if (!MvhdAtomHeaderNamePassed()) continue;
 
+                    usefulByteCount++;
 
+                    if (usefulByteCount > USEFUL_BYTE_OFFSET && usefulByteCount <= USEFUL_BYTE_OFFSET + 4)
+                    {
+                        timeScaleBytes[timeScaleBytesIndex] = _bytes[_numBytesRead - 1];
+                        timeScaleBytesIndex++;
+                    }
+
+                    if (usefulByteCount > USEFUL_BYTE_OFFSET + 4 && usefulByteCount <= USEFUL_BYTE_OFFSET + 8)
+                    {
+                        durationBasedOnTimeScaleBytes[durationBasedOnTimeScaleBytesIndex] = _bytes[_numBytesRead - 1];
+                        durationBasedOnTimeScaleBytesIndex++;
+                    }
+
+                    if (usefulByteCount > USEFUL_BYTE_OFFSET + 8)
+                        break;
                 }
             }
-        }
 
-        private bool MvhdAtomFound()
-        {
-            for (int i = 4; i < _numBytesRead; i++)
+            if (BitConverter.IsLittleEndian)
             {
-                if (_bytes[i - 3] == 0x6C && _bytes[i - 2] == 0x6D && _bytes[i - 1] == 0x76 && _bytes[i] == 0x68)
-                    return true;
+                Array.Reverse(timeScaleBytes);
+                Array.Reverse(durationBasedOnTimeScaleBytes);
             }
 
-            return false;
+            int timeScale = BitConverter.ToInt32(timeScaleBytes, 0);
+            int durationBasedOnTimeScale = BitConverter.ToInt32(durationBasedOnTimeScaleBytes, 0);
+
+            return (double)durationBasedOnTimeScale / timeScale;
+        }
+
+        private bool MvhdAtomHeaderNamePassed()
+        {
+            if (_numBytesRead < MVHD_HEADER_NAME_LENGTH)
+                return false;
+
+            if (_bytes[_numBytesRead - 2] == 0x64 && _bytes[_numBytesRead - 3] == 0x68 && _bytes[_numBytesRead - 4] == 0x76 && _bytes[_numBytesRead - 5] == 0x6D)
+                _mvhdAtomHeaderNamePassed = true;
+
+            return _mvhdAtomHeaderNamePassed;
         }
     }
 }
---
+++ tests/Oxigen.Tests/Oxigen.DurationDetectors/MovieDurationTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using Oxigen.DurationDetectors;
+
+namespace Tests.Oxigen.Web
+{
+    [TestFixture, Category("IntegrationTest")]
+    public class MovieDurationTests
+    {
+        private double TIME_THRESHOLD_SECONDS = 0.5;
+
+        [Test]
+        public void CanGetCorrectQuicktimeDuration1()
+        {
+            // Arrange
+            QuicktimeFileDurationDetector qtDetector = new QuicktimeFileDurationDetector();
+            double actualDuration;
+            double expectedDuration = 85.5;
+
+            // Act
+            actualDuration = qtDetector.GetDurationInSeconds(@"../../FileSystemResources/sample.mov");
+
+            // Assert
+            AssertWithThreshold(expectedDuration, actualDuration, TIME_THRESHOLD_SECONDS);
+        }
+
+        [Test]
+        public void CanGetCorrectQuicktimeDuration2()
+        {
+            // Arrange
+            QuicktimeFileDurationDetector qtDetector = new QuicktimeFileDurationDetector();
+            double actualDuration;
+            double expectedDuration = 45.07;
+
+            // Act
+            actualDuration = qtDetector.GetDurationInSeconds(@"../../FileSystemResources/MonteCarloSundayHotshot.mov");
+
+            // Assert
+            AssertWithThreshold(expectedDuration, actualDuration, TIME_THRESHOLD_SECONDS);
+        }
+
+        [Test]
+        public void CanGetCorrectQuicktimeDuration3()
+        {
+            // Arrange
+            QuicktimeFileDurationDetector qtDetector = new QuicktimeFileDurationDetector();
+            double actualDuration;
+            double expectedDuration = 38;
+
+            // Act
+            actualDuration = qtDetector.GetDurationInSeconds(@"../../FileSystemResources/Madrid_20110505_Hotshot.mov");
+
+            // Assert
+            AssertWithThreshold(expectedDuration, actualDuration, TIME_THRESHOLD_SECONDS);
+        }
+
+        private void AssertWithThreshold(double expectedDuration, double actualDuration, double timeThreshold)
+        {
+            if (Math.Abs(expectedDuration - actualDuration) <= timeThreshold)
+                Assert.Pass("Difference between actual and expected values is within the threshold of " + timeThreshold);
+
+            Assert.Fail("Difference between actual and expected values is outside the threshold of " + timeThreshold);
+        }
+    }
+}
