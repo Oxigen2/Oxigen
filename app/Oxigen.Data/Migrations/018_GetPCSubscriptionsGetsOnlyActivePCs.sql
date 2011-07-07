9032237b222faffbbc57291eb88e535e4a2fd7f6
Michali K <michali@gmail.com>
2011-07-07 19:13:53 +0100

    stored procedure only gets active pcs. Was a bug with the download page where even the deleted PCs showed.

---
+++ app/Oxigen.Data/Migrations/018_GetPCSubscriptionsGetsOnlyActivePCs.sql
@@ -0,0 +1,25 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[dp_getPcSubscriptions]
+(
+@UserID int
+)
+
+AS
+
+SELECT
+	PCs.PC_ID,
+	PCs.PcName,
+	Channels.CHANNEL_ID,
+	Channels.ChannelGUID,
+	Channels.ChannelName,	
+	PcChannels.ChannelWeighting
+FROM
+	PCs INNER JOIN PcChannels ON PCs.PC_ID = PcChannels.PcID
+	INNER JOIN Channels ON Channels.CHANNEL_ID = PcChannels.ChannelID
+	INNER JOIN Consumers ON PCs.ConsumerID = Consumers.CONSUMER_ID
+WHERE
+	Consumers.UserID = @UserID
+	AND PCs.bDeleted = 0;
